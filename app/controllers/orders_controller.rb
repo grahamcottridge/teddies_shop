3cef9296d2d4dc58fa49ac0485839400613b5030
Graham Cottridge <codinggc@gmail.com>
2017-11-27 20:54:39 +0000

    Add Payments controller and views

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   def show
+    @order = Order.where(state: 'paid').find(params[:id])
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root 'teddies#index'
   resources :teddies, only: [:index, :show]
 
-  resources :orders, only: [:show, :create]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :orders, only: [:show, :create] do
+    resources :payments, only: [:new, :create]
+  end
+  
 end

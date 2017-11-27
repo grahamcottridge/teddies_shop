bb02f6af3db0d051cfc1d6d6f7481480a66e91be
Graham Cottridge <codinggc@gmail.com>
2017-11-27 16:44:00 +0000

    Create model and add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Teddy.destroy_all
+Category.destroy_all
+
+puts 'Creating categories...'
+geek = Category.create!(name: 'geek')
+kids = Category.create!(name: 'kids')
+
+puts 'Creating teddies...'
+Teddy.create!(sku: 'original-teddy-bear', name: 'Teddy bear', category: kids, photo_url: 'http://onehdwallpaper.com/wp-content/uploads/2015/07/Teddy-Bears-HD-Images.jpg')
+
+Teddy.create!(sku: 'jean-mimi', name: 'Jean-Michel - Le Wagon', category: geek, photo_url: 'https://pbs.twimg.com/media/B_AUcKeU4AE6ZcG.jpg:large')
+Teddy.create!(sku: 'octocat',   name: 'Octocat -  GitHub',      category: geek, photo_url: 'https://cdn.shopify.com/s/files/1/0051/4802/products/mona-2_1024x1024.jpg?v=1447180277')
+puts 'Finished!'
